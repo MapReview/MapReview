50e854603f56605276ec38bf42e742561a3512f9
Mark Kimball <parkskier@gmail.com>
2015-07-10 16:17:32 -0400

    some commits

--- app/Map/service.js
+++ app/Map/service.js
@@ -3,6 +3,17 @@
   angular
         .module('map')
         .factory('MapService', function($http) {
+          var reviewsUrl = "http://tiy-fee-rest.herokuapp.com/collections/MapReview1"
 
+          var getMarkers = function() {
+            return $http.get(reviewsUrl).then(function(reviews){
+              console.log(reviews.data);
+              return reviews.data;
+            })
+          };
+
+          return {
+            getMarkers: getMarkers
+          }
     });
 })();
