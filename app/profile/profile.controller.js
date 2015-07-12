d77042687cb6a96d7c1ac1810c33b3a95142c3ac
Trevor Glass <glasser1988@gmail.com>
2015-07-12 16:24:50 -0400

    not to lose

--- app/profile/profile.controller.js
+++ app/profile/profile.controller.js
@@ -31,7 +31,7 @@ angular.module('profile')
     $scope.updateProfile = function() {
       Account.updateProfile({
         displayName: $scope.user.displayName,
-        email: $scope.user.email
+        email: $scope.user.email,
       }).then(function() {
         $alert({
           content: 'Profile has been updated',
@@ -101,6 +101,14 @@ angular.module('profile')
   angular.module('profile')
     .controller('ProgressDemoCtrl', function ($scope) {
       $scope.max = 200;
+
+    $scope.random = function() {
+    var value = Math.floor((Math.random() * 100) + 1);
+
+    $scope.dynamic = value;
+  };
+  $scope.random();
+
     });
 
   angular.module('profile')
